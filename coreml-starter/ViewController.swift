04e2476b3917d1a96e9548c106c8d4bb7328cb4a
Dmitry Pavlenko <basket.pavlenko@gmail.com>
2018-09-26 12:29:02 +0300

    Initial Commit

---
+++ coreml-starter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  coreml-starter
+//
+//  Created by Dmytro Pavlenko on 9/26/18.
+//  Copyright © 2018 Dmytro Pavlenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+
+}
+
+
---
+++ coreml-starterTests/coreml_starterTests.swift
@@ -0,0 +1,34 @@
+//
+//  coreml_starterTests.swift
+//  coreml-starterTests
+//
+//  Created by Dmytro Pavlenko on 9/26/18.
+//  Copyright © 2018 Dmytro Pavlenko. All rights reserved.
+//
+
+import XCTest
+@testable import coreml_starter
+
+class coreml_starterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
